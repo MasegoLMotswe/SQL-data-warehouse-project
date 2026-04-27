9074d0ffa20568138fe7487595f2a3a0739a80e5
Masego Lesedi Motswe <masegol.motswe@gmail.com>
2026-04-27 18:28:16 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,20 @@
-
+-- checks database exists then drops it if it does and creates a new one WILL DROP IF EXISTS!!!!!!!!
+USE master;
+GO
+IF EXISTS(SELECT 1 FROM sys.database WHERE name = 'MDataWarehouse')
+BEGIN 
+  ALTER DATABASE MDataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE MDataWarehouse
+END;
+GO  
+  CREATE DATABASE MDataWarehouse;
+GO
+  USE MDataWarehouse;
+GO
+--CREATE SCHEMAS 
+CREATE SCHEMA bronze;
+  GO
+CREATE SCHEMA sliver;
+  GO
+CREATE SCHEMA gold;
+  GO
